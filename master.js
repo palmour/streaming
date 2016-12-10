6779f285493f4f009ddf60ef85fddcb0560f6f20
Christopher Palmour <palmour@live.unc.edu>
2016-12-09 21:18:23 -0500

    Add sign out function on master library page.

--- master.js
+++ master.js
@@ -85,6 +85,21 @@ $(document).ready(function(){
         });
     });
 
+    $("#sign-out").click(function(){
+        $.ajax("backend/logout.php", 
+        {type: "GET",
+            dataType: 'json',
+            cache: false,
+            success: function(){
+                window.location.assign('signin.html');
+            }, 
+            error: function(){
+                alert('logout failed');
+            }
+
+        });
+    });
+
     library_tab.click(function(){
         window.location.assign("main.html");
     });
